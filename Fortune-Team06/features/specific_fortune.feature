1135857c08b1fe6054692fbedac0634e5a1e222e
unknown <localmgr@.rose-hulman.edu>
2011-05-09 14:13:38 -0400

    added all files

---
+++ Fortune-Team06/features/specific_fortune.feature
@@ -0,0 +1,8 @@
+Feature: Specific fortune
+	In order to receive enlightenment
+	As a depressed person
+	I want to be given a specific fortune
+	
+Scenario: Get a fortune
+	Given I am on fortunes
+	Then I should see a fortune with ID
---
+++ Fortune-Team06/fortune.rb
@@ -0,0 +1,14 @@
+require 'sinatra/base'
+require 'haml'
+require 'thin'
+class Fortune < Sinatra::Base
+	get '/fortune/random/?' do
+		@fortune = $fortunes[rand($fortunes.size)]
+		haml :fortune
+	end
+	get '/fortune/get/:fid' do
+		@fortune = $fortunes[params[:fid].to_i]
+		haml :fortune
+	end
+end
+require 'fortunedata'
